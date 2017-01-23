69e482c500bd814a0ea127ffb5e70ca1146535f4
zhanghao639 <zhanghao639@pingan.com.cn>
2017-01-23 15:54:09 +0800

    commit

--- timepicker.js
+++ timepicker.js
@@ -23,6 +23,33 @@ var TimePicker = Class.$factory('timepicker', {
 
         this.create();
         this.createSelector();
+        this.initEvent();
+    },
+
+    initEvent: function(){
+        var self = this;
+
+        self.$wraper.delegate('.ui3-timepicker-si', 'click', function(){
+            var $item = $(this);
+
+            self.$wraper.find('.ui3-timepicker-si').removeClass('ui3-timepicker-si-selected');
+            $item.addClass('ui3-timepicker-si-selected');
+            self.$inputs.eq($item.attr('data-type')).val($item.attr('data-value'));
+        });
+
+        self.$inputs.each(function(index){
+            $(this).click(function(){
+                self.createSelector(index);
+            });
+        });
+
+        self.$wraper.delegate('.ui3-timepicker-selector-closer', 'click', function(){
+            self.close();
+        });
+
+        self.$wraper.find('.ui3-timepicker-confirm').click(function(){
+            self.trigger('select', self.getTime());
+        });
     },
 
     create: function(){
@@ -32,7 +59,7 @@ var TimePicker = Class.$factory('timepicker', {
             <div class="ui3-timepicker">\
                 <div class="ui3-timepicker-selector"></div>\
                 <div class="ui3-timepicker-vs">\
-                    <input type="text" class="ui3-timepicker-hours" /><i>:</i><input type="text" class="ui3-timepicker-minutes" /><i>:</i><input type="text" class="ui3-timepicker-seconds" />\
+                    <input type="text" class="ui3-timepicker-hours" readonly value="00" /><i>:</i><input type="text" class="ui3-timepicker-minutes" readonly value="00" /><i>:</i><input type="text" class="ui3-timepicker-seconds" readonly value="00" />\
                     <a href="javascript:" class="ui3-timepicker-confirm">确定</a>\
                 </div>\
             </div>\
@@ -42,24 +69,32 @@ var TimePicker = Class.$factory('timepicker', {
             self.$overlay = new Overlay();
             self.$overlay.setContent(self.$wraper);
         }else{
-            this.open();
+            self.$wraper.appendTo(options.container);
         }
+
+        self.$inputs = self.$wraper.find('input');
     },
 
     createSelector: function(type/*0|1|2*/){
         type = type || 0;
 
         var end = type == 0 ? 23: 59;
-        var txt, htmls = ['<div class="ui3-timepicker-selector-title"><a href="javascript:" class="ui3-timepicker-selector-closer">&times;</a>' + TimePicker.TYPES[type] + '</div>'];
+        var txt, htmls = [];
         var self = this;
+        var v = self.getTime(type);
 
         for(var i = 0; i <= end; i++){
             txt = i < 10 ? '0' + i : i;
-            htmls.push('<a href="javascript:" class="ui3-timepicker-si">' + txt + '</a>');
+            htmls.push('<a href="javascript:" class="ui3-timepicker-si ' + (i == v ? 'ui3-timepicker-si-selected' : '') + '" data-type="' + type + '" data-value="' + txt + '">' + txt + '</a>');
         }
 
-        var $selector = self.$wraper.find('.ui3-timepicker-selector');
-        $selector.html(htmls.join(''));
+        var $selector = self.$wraper.find('.ui3-timepicker-selector').show();
+
+        htmls = '<div class="ui3-timepicker-selector-title"><a href="javascript:" class="ui3-timepicker-selector-closer">&times;</a>' + TimePicker.TYPES[type] + '</div>'
+                + '<div class="ui3-timepicker-selector-wraper">'
+                + htmls.join('')
+                + '</div>';
+        $selector.html(htmls);
 
         if(type == 0){
             $selector.addClass('ui3-timepicker-selector-hours');
@@ -69,10 +104,39 @@ var TimePicker = Class.$factory('timepicker', {
     },
 
     open: function(){
-        this.$wraper.appendTo(this.options.container);
+        var self = this;
+
+        if(self.$overlay){
+            self.$overlay.open();
+        }else{
+            self.$wraper.find('.ui3-timepicker-selector').show();
+        }
     },
+
     close: function(){
-        this.$wraper.remove();
+        var self = this;
+
+        if(self.$overlay){
+            self.$overlay.close();
+        }else{
+            self.$wraper.find('.ui3-timepicker-selector').hide();
+        }
+    },
+
+    getTime: function(type){
+        var self = this;
+
+        if(type == null){
+            var vs = [];
+
+            self.$inputs.each(function(){
+                vs.push(this.value);
+            });
+
+            return vs.join(':');
+        }else{
+            return self.$inputs.eq(type).val();
+        }
     }
 });
 
